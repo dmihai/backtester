504ac80eba7ef88a5991f17bc81acbfa4752edda
Mihai Doarna <md@zeuz.io>
2022-06-02 12:05:42 +0300

    add basic job processing

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import config
+import sys
+import importlib
 from utils.Database import Database
+
 
 db = Database()
 db.connect(config.db_host, config.db_user, config.db_pass, config.db_name)
-db.assign_job_to_worker(config.worker_id)
+
 job = db.get_next_processing_job_by_worker(config.worker_id)
+if job is None:
+    db.assign_job_to_worker(config.worker_id)
+    job = db.get_next_processing_job_by_worker(config.worker_id)
+
+if job is None:
+    sys.exit()
+
 print(job)
+
+module = importlib.import_module("backtesting." + job['strategy'])
+class_ = getattr(module, job['strategy'])
+test = class_(**job['params'])
+test.test()
+print(test.get_groupby_status())
+print(f"Init exexution time {test.get_init_execution_time()}")
+print(f"Test execution time {test.get_test_execution_time()}")
