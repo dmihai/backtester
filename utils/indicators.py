6319a297be85ad6994891bff03cdaeb2d374f9ac
Mihai Doarna <dmihai@gmail.com>
2022-10-07 12:29:01 +0300

    add rsi indicator

--- utils/indicators.py
+++ utils/indicators.py
@@ -22,3 +22,23 @@ def add_resistance(df, max_radius=200):
                'resistance_right'] = i
 
     return df
+
+def add_rsi(df, periods=14, column='close_price', ema=True):
+    close_delta = df[column].diff()
+
+    up = close_delta.clip(lower=0)
+    down = -1 * close_delta.clip(upper=0)
+
+    if ema == True:
+       # use exponential moving average (ema)
+       ma_up = up.ewm(com=periods-1, adjust=True, min_periods=periods).mean()
+       ma_down = down.ewm(com=periods-1, adjust=True, min_periods=periods).mean()
+    else:
+       # use simple moving average (sma)
+       ma_up = up.rolling(window=periods, adjust=False).mean()
+       ma_down = down.rolling(window=periods, adjust=False).mean()
+
+    rsi = ma_up / ma_down
+    df['rsi'] = 100 - (100 / (1 + rsi))
+
+    return df
