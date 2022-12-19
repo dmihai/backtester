d951b63eaa7e29f196c4a826d22b5a60e2511329
Mihai Doarna <dmihai@gmail.com>
2022-12-19 10:47:23 +0200

    DoubleTopDoubleBottom: prepare data

--- backtesting/strategies/DoubleTopDoubleBottom.py
+++ backtesting/strategies/DoubleTopDoubleBottom.py
@@ -23,6 +23,59 @@ class DoubleTopDoubleBottom(Backtester):
         df = add_support(df)
         df = add_resistance(df)
 
+        df['body_low_price'] = df[['close_price', 'open_price']].min(axis=1)
+        df['body_high_price'] = df[['close_price', 'open_price']].max(axis=1)
+
+        df['swing_low_wick'] = 0.0
+        df['swing_low_body'] = 0.0
+        df['swing_low_left'] = 0
+        df['swing_low_right'] = 0
+        df['swing_high_wick'] = 0.0
+        df['swing_high_body'] = 0.0
+        df['swing_high_left'] = 0
+        df['swing_high_right'] = 0
+
+        def compute_swing_params(df):
+            if df.support_left >= 2 and df.support_right >= 2:
+                df['swing_low_wick'] = df.low_price
+                df['swing_low_body'] = df['body_low_price']
+                df['swing_low_left'] = 1
+                df['swing_low_right'] = 1
+            
+            if df.resistance_left >= 2 and df.resistance_right >= 2:
+                df['swing_high_wick'] = df.high_price
+                df['swing_high_body'] = df['body_high_price']
+                df['swing_high_left'] = 1
+                df['swing_high_right'] = 1
+            
+            return df
+
+        df = df.apply(compute_swing_params, axis=1)
+
+        for i in range(1, 10):
+            # check if swing structure is valid
+            # (zone between swing wick and swing body has room to left and right)
+            df.loc[(df.swing_low_left == 1) & ((df.shift(i).low_price > df.swing_low_body)), 'swing_low_left'] = 2
+            df.loc[(df.swing_low_right == 1) & ((df.shift(-i).low_price > df.swing_low_body)), 'swing_low_right'] = 2
+            df.loc[(df.swing_high_left == 1) & ((df.shift(i).high_price < df.swing_high_body)), 'swing_high_left'] = 2
+            df.loc[(df.swing_high_right == 1) & ((df.shift(-i).high_price < df.swing_high_body)), 'swing_high_right'] = 2
+
+            # check if swing structure is invalid
+            df.loc[(df.swing_low_left == 1) & ((df.shift(i).low_price < df.swing_low_wick)), 'swing_low_left'] = 3
+            df.loc[(df.swing_low_right == 1) & ((df.shift(-i).low_price < df.swing_low_wick)), 'swing_low_right'] = 3
+            df.loc[(df.swing_high_left == 1) & ((df.shift(i).high_price > df.swing_high_wick)), 'swing_high_left'] = 3
+            df.loc[(df.swing_high_right == 1) & ((df.shift(-i).high_price > df.swing_high_wick)), 'swing_high_right'] = 3
+
+            # update swing body
+            df.loc[(df.swing_low_left == 1) & (df.shift(i).body_low_price < df.swing_low_body), 'swing_low_body'] = df.shift(i).body_low_price
+            df.loc[(df.swing_low_right == 1) & (df.shift(-i).body_low_price < df.swing_low_body), 'swing_low_body'] = df.shift(-i).body_low_price
+            df.loc[(df.swing_high_left == 1) & (df.shift(i).body_high_price > df.swing_high_body), 'swing_high_body'] = df.shift(i).body_high_price
+            df.loc[(df.swing_high_right == 1) & (df.shift(-i).body_high_price > df.swing_high_body), 'swing_high_body'] = df.shift(-i).body_high_price
+        
+        print(df)
+        print(df.loc[((df.swing_low_left == 2) & (df.swing_low_right == 2)) | ((df.swing_high_left == 2) & (df.swing_high_right == 2))])
+        print(df.loc[(df.swing_low_left == 3) | (df.swing_low_right == 3) | (df.swing_high_left == 3) | (df.swing_high_right == 3)])
+
         return df
     
     def _calculate_triggers(self):
