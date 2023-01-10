82d899e5c48314be8f845e862759a3ec5dc146ee
Mihai Doarna <dmihai@gmail.com>
2023-01-10 17:40:23 +0200

    add room to left param to engulfing function

--- utils/patterns.py
+++ utils/patterns.py
@@ -45,21 +45,31 @@ def add_kangaroo(data, pip_value=0.0001, min_pips=20, pin_divisor=3.0, room_left
 
 # Marks bullish and bearish engulfing candles
 # Make sure that the dataframe contains body_low_price and body_high_price
-def add_engulfing(data, pip_value=0.0001, min_pips=10):
-       df = data.copy()
+def add_engulfing(data, pip_value=0.0001, min_pips=10, room_left=4, room_divisor=5.0):
+    df = data.copy()
 
-       min_length = min_pips * pip_value
+    df['engulfing'] = 0
 
-       # bearish engulfing
-       df.loc[(df.body_low_price < df.shift(1).body_low_price) &
-              (df.body_high_price >= df.shift(1).body_high_price) &
-              (df.open_price > df.close_price) &
-              (df.open_price - df.close_price > min_length) , 'engulfing'] = -1
-       
-       # bullish engulfing
-       df.loc[(df.body_low_price <= df.shift(1).body_low_price) &
-              (df.body_high_price > df.shift(1).body_high_price) &
-              (df.close_price > df.open_price) &
-              (df.close_price - df.open_price > min_length) , 'engulfing'] = 1
+    min_length = min_pips * pip_value
+    room_length = (df.high_price - df.low_price) / room_divisor
 
-       return df
+    # bearish engulfing
+    df.loc[(df.body_low_price < df.shift(1).body_low_price) &
+           (df.body_high_price >= df.shift(1).body_high_price) &
+           (df.open_price > df.close_price) &
+           (df.open_price - df.close_price > min_length) , 'engulfing'] = -1
+
+    # bullish engulfing
+    df.loc[(df.body_low_price <= df.shift(1).body_low_price) &
+           (df.body_high_price > df.shift(1).body_high_price) &
+           (df.close_price > df.open_price) &
+           (df.close_price - df.open_price > min_length) , 'engulfing'] = 1
+
+    # check if there is space to the left
+    for i in range(2, room_left+2):
+        df.loc[(df.engulfing == -1) & (df.shift(i).high_price > df.high_price - room_length) & (df.shift(i).high_price > df.shift(1).high_price - room_length),
+               'engulfing'] = 0
+        df.loc[(df.engulfing == 1) & (df.shift(i).low_price < df.low_price + room_length) & (df.shift(i).low_price < df.shift(1).low_price + room_length),
+               'engulfing'] = 0
+
+    return df
